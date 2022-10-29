1f9bdd00a15594ce1ec717db8126a2d64beaee0c
Avgust Varlamov <varl.avgust@yandex.ru>
2022-10-29 14:01:10 +0300

    fix some bugs

--- quadEquAnalysis.c
+++ quadEquAnalysis.c
@@ -3,43 +3,44 @@
 //
 
 #include <math.h>
-
+#include <stddef.h>
+#include "quadEquAnalysis.h"
 // status enumerator for check all moments
-enum status {TWO_REAL_ROOTS, ONE_MULT_ROOT, TWO_COMPLEX_ROOTS,
-    ONE_ROOT_LINEAR_EQUATION, NO_ROOTS, ANY_REAL_ROOT,
-    INVALID_COEFFS};
+//enum status {TWO_REAL_ROOTS, ONE_MULT_ROOT, TWO_COMPLEX_ROOTS,
+//    ONE_ROOT_LINEAR_EQUATION, NO_ROOTS, ANY_REAL_ROOT,
+//    INVALID_COEFFS};
 
 
 enum status quadEquAnalysis(double a, double b, double c,
         double *x1, double *x2, double *cxa, double *cxb) {
     double d, sqrt_d;
-    d = b * b - 4 * a * c; // discriminant
 
     // count roots if it's possible
-    if (isnan(a) || a == 0.0/0.0 ||
-        isnan(b) || b == 0.0/0.0 ||
-        isnan(c) || c == 0.0/0.0)
+    if (isnan(a) || isinf(c) ||
+        isnan(b) || isinf(b) ||
+        isnan(c) || isinf(c))
         return INVALID_COEFFS;
     else if (a != 0) {
+        d = b * b - 4 * a * c; // discriminant
         if (d > 0) {
             sqrt_d = sqrt(d);
-            *x1 = (-b + sqrt_d) / 2 / a;
-            *x2 = (-b - sqrt_d) / 2 / a;
+            if (x1 != NULL) *x1 = (-b + sqrt_d) / 2 / a;
+            if (x2 != NULL) *x2 = (-b - sqrt_d) / 2 / a;
             return TWO_REAL_ROOTS;
         }
         else if (d == 0) {
-            *x1 = -b / 2 / a;
+            if (x1 != NULL) *x1 = -b / 2 / a;
             return ONE_MULT_ROOT;
         }
         else if (d < 0) {
             sqrt_d = sqrt(-d);
-            *cxa = -b / 2 / a;
-            *cxb = sqrt_d / 2 / a;
+            if (cxa != NULL) *cxa = -b / 2 / a;
+            if (cxb != NULL) *cxb = sqrt_d / 2 / a;
             return TWO_COMPLEX_ROOTS;
         }
     }
     else if (b != 0) {
-        *x1 = -c / b;
+        if (x1 != NULL) *x1 = -c / b;
         return ONE_ROOT_LINEAR_EQUATION;
     }
     else if (c != 0) return NO_ROOTS;
--- quadEquAnalysis.h
+++ quadEquAnalysis.h
@@ -3,5 +3,6 @@
 enum status {TWO_REAL_ROOTS, ONE_MULT_ROOT, TWO_COMPLEX_ROOTS,
     ONE_ROOT_LINEAR_EQUATION, NO_ROOTS, ANY_REAL_ROOT,
     INVALID_COEFFS};
-enum status quadEquAnalysis();
+enum status quadEquAnalysis(double a, double b, double c,
+                            double *x1, double *x2, double *cxa, double *cxb);
 #endif
